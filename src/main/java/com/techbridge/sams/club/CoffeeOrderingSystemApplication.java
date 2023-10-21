91f29eb0b766fe2b404e9fc79b4affc4ed0683a7
Elizabeth <kenkwoelizabeth78@gmail.com>
2023-10-21 17:42:55 -0400

    first commit

---
+++ src/main/java/com/techbridge/sams/club/CoffeeOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.techbridge.sams.club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeOrderingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techbridge/sams/club/CoffeeOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbridge.sams.club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
